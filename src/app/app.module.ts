4f898da02ddf4e14310e8f3c11f36ebd5576b529
benrouba <benrouba.ferdaous@gmail.com>
2023-06-17 15:49:36 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,91 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClient, HttpClientModule, } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// ************************************************************************ngx bootstrap ************************************************************************
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
+// ************************************************************************Tools************************************************************************
+
+import { NgOtpInputModule } from 'ng-otp-input';
+// ************************************************************************Guards************************************************************************
+
+import { authGuard } from './guards/auth.guard';
+
+// ************************************************************************Components************************************************************************
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginSignUpComponent } from './reusable/login-sign-up/login-sign-up.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { VerificationCodeComponent } from './components/verification-code/verification-code.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { DashboardComponent } from './components/parent/dashboard/dashboard.component';
+import { MedicalHistoryComponent } from './components/parent/medical-history/medical-history.component';
+import { ProfileComponent } from './components/parent/profile/profile.component';
+import { MyPatientsComponent } from './components/parent/my-patients/my-patients.component';
+import { HeaderComponent } from './reusable/header/header.component';
 
+
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "sign-up",
+    component: SignUpComponent
+  },
+  {
+    path: "forget-password",
+    component: ForgetPasswordComponent
+  },
+  {
+    path: "verification-code",
+    component: VerificationCodeComponent
+  },
+  {
+    path: "",
+    component: ParentComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: "dashboard", component: DashboardComponent },
+      { path: "medical-history", component: MedicalHistoryComponent },
+      { path: "my-patients", component: MyPatientsComponent },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ]
+  },
+  { path: "profile", component: ProfileComponent, canActivate: [authGuard] },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginSignUpComponent,
+    SignUpComponent,
+    ForgetPasswordComponent,
+    VerificationCodeComponent,
+    ParentComponent,
+    DashboardComponent,
+    MedicalHistoryComponent,
+    MyPatientsComponent,
+    ProfileComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    PaginationModule.forRoot(),
+    NgOtpInputModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
+  providers: [authGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  endpoint: 'https://dummyjson.com/',
+};
