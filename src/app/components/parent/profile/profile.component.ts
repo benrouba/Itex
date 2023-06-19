976788b3ec50bbb170998d4c7f676fd1f52a97f2
benrouba <benrouba.ferdaous@gmail.com>
2023-06-19 08:33:04 -0600

    finish the profile component

--- src/app/components/parent/profile/profile.component.ts
+++ src/app/components/parent/profile/profile.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  imgUrl: any
+  selectedFile: any = null;
+  ngOnInit(): void {
 
+  }
+  showPreviewImage(event: any,) {
+    console.log(event);
+
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.imgUrl = event.target.result;
+      };
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedFile = <File>event.target.files[0];
+    }
+  }
 }
